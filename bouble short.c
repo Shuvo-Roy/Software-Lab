fa42a2e5eb0fbfba95c70b03523d00fee1f6fb8c
Shuvo Roy <shuvoroyspd@gmail.com>
2019-07-08 14:42:38 +0600

    dddd

--- bouble short.c
+++ bouble short.c
@@ -2,24 +2,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,temp;
-     printf("Enter Number of elements in an array:");
-    scanf("%d",&n);
-    int a[n];
-    printf(" Enter %d elements of an Array :", n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
 
-    for(i=0;i<n;i++)
     {
         for(j=0;j<n-i;j++)
         {
             if(a[j]>a[j+1])
             {
                 temp=a[j];
-                a[j]=a[j+1];
+
                 a[j+1]=temp;
             }
         }
